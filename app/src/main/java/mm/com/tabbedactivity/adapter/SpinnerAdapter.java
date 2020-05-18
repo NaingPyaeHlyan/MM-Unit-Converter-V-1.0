bd5dc15514201b96b75fb2c34b9413409ff5c168
nph <naingpyaehlyan@gmail.com>
2020-05-18 12:19:20 +0630

    change to unicode

--- app/src/main/java/mm/com/tabbedactivity/adapter/SpinnerAdapter.java
+++ app/src/main/java/mm/com/tabbedactivity/adapter/SpinnerAdapter.java
@@ -21,7 +21,7 @@ public class SpinnerAdapter extends ArrayAdapter<String> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         View view = super.getView(position, convertView, parent);
-        Typeface typeface = Typeface.createFromAsset(context.getAssets(), "zawgyione.ttf");
+        Typeface typeface = Typeface.createFromAsset(context.getAssets(), "pyidaungsu.ttf");
         ((TextView) view).setTypeface(typeface);
 
         return view;
@@ -30,7 +30,7 @@ public class SpinnerAdapter extends ArrayAdapter<String> {
     @Override
     public View getDropDownView(int position, View convertView, ViewGroup parent){
         View view = super.getView(position, convertView, parent);
-        Typeface typeface = Typeface.createFromAsset(context.getAssets(), "zawgyione.ttf");
+        Typeface typeface = Typeface.createFromAsset(context.getAssets(), "pyidaungsu.ttf");
         ((TextView) view).setTypeface(typeface);
 
         return view;
